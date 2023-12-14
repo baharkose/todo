60bf92ec96bdd918ff0b1ca469f63700b546e10a
baharkose <baharkse17@gmail.com>
2023-12-15 01:46:12 +0300

    added empty image

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import "./App.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import background from "./img/back.png";
-
+import emptyImg from "./img/empty.png";
 
 function App() {
   const [addList, setAddList] = useState(
     JSON.parse(localStorage.getItem("list")) || []
   );
+  const listLength = addList.length;
+  console.log(listLength)
 
   return (
     <>
       <div className="container">
         <div className="todo-container">
-          <h1>To Do List</h1>
+          <h1 style={{ color: "#5B5A76" }}>To Do List</h1>
           <Header setAddList={setAddList} addList={addList} />
           <div>
+            <div
+              className="emptyResim"
+              style={{ display: listLength === 0 ? "block" : "none" }}
+            >
+              <img className="emptyImg" src={emptyImg} alt="" width="170px" />
+            </div>
             <TodoList addList={addList} setAddList={setAddList} />
           </div>
 
